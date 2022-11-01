90f55588a889abf15d7483ffcea2624c31e11ede
MohammedSiddiqui747 <114967573+MohammedSiddiqui747@users.noreply.github.com>
2022-10-31 20:49:47 -0400

    testing new file adding method
    adding functionality files for partner to use.

---
+++ AddList.js
@@ -0,0 +1,52 @@
+import { render } from '@testing-library/react';
+import React, { useEffect } from 'react'
+
+// const addedMeals = [{title : "apple pie"}, {title: "steak"}];
+
+class AddList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.mealData = this.props.mealData
+    this.userAddedMeals = this.props.userAddedMeals
+    this.test = this.props.test
+    this.options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '220e5cfb44mshce089b2540207f9p1cb4f9jsne3e1c3089e33',
+        'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+      }
+    };
+  }
+
+  getMealCal(meal) {
+    // let nutrients = this.mealData.filter((m) => m.id == meal.id)[0].nutrients
+    console.log(this.mealData)
+    return 0
+  }
+  getMealFat(meal) {
+    // let nutrients = this.mealData.filter((m) => m.id == meal.id)[0].nutrients
+    return 0
+  }
+
+  render() {
+    this.userAddedMeals.forEach(meal => {
+
+    });
+
+    return (
+        <div className="mealInfo">
+            {this.userAddedMeals.map((meal) => {
+              return <li key={meal.title}>{meal.title}: <br/>
+              Cal:{meal.calories}, <br/>Fat: {meal.fat}</li>
+            })}
+        </div>
+    )
+  }
+  }
+  
+  export default AddList
+
+
+// track meals
+// way to add meals
+// link this to the add button
